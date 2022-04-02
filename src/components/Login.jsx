62042603ccf2f703756af1f35c63fe1c9d61bc92
Thomas LY <thomas.ly@iologo.io>
2022-04-02 14:04:50 +0200

    test 2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const navigate = useNavigate();
   const responseGoogle = (response) => {
     localStorage.setItem('user', JSON.stringify(response.profileObj));
-    console.log(response.profileObj)
+    console.log(response)
     const { name, googleId, imageUrl } = response.profileObj;
     const doc = {
       _id: googleId,
